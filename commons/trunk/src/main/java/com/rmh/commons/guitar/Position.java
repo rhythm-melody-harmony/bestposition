78a4dc994b802d3e2a977d32ca8e59c04e7a8a6f
derado <derado@acbfa5e8-46c2-402e-9d77-156e8e9eb09e>
2012-10-16 20:10:31 +0000

    Finger added to guitar position

--- commons/trunk/src/main/java/com/rmh/commons/guitar/Position.java
+++ commons/trunk/src/main/java/com/rmh/commons/guitar/Position.java
@@ -7,11 +7,13 @@ public class Position {
     private Tone tone;
     private int fret;
     private int string;
+    private int finger;
     
-    public Position(Tone tone, int fret, int string) {
+    public Position(Tone tone, int fret, int string, int finger) {
         this.tone = tone;
         this.fret = fret;
         this.string = string;
+        this.finger = finger;
     }
 
     public Tone getTone() {
@@ -24,6 +26,10 @@ public class Position {
 
     public int getString() {
         return string;
+    }
+
+    public int getFinger() {
+        return finger;
     }
     
 }
