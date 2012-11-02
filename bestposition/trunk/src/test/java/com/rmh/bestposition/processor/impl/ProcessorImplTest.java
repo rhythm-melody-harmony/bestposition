5dafd700634ca51214fe24250bbb001c23884ed8
jocca1985 <jocca1985@acbfa5e8-46c2-402e-9d77-156e8e9eb09e>
2012-11-02 01:35:51 +0000

    Test for Proccessor implementation

--- bestposition/trunk/src/test/java/com/rmh/bestposition/processor/impl/ProcessorImplTest.java
+++ bestposition/trunk/src/test/java/com/rmh/bestposition/processor/impl/ProcessorImplTest.java
@@ -1,11 +1,15 @@
 package com.rmh.bestposition.processor.impl;
 
 import com.rmh.bestposition.processor.Processor;
+
 import com.rmh.bestposition.utils.DataBuilder;
 import com.rmh.commons.Tone;
 import com.rmh.commons.guitar.FretBoard;
 import com.rmh.commons.guitar.Position;
 import com.rmh.commons.guitar.PositionPoint;
+import com.rmh.commons.guitar.ToneEdge;
+import com.rmh.commons.guitar.ToneVertex;
+import java.util.ArrayList;
 import java.util.List;
 import static org.junit.Assert.*;
 import org.junit.Test;
@@ -15,12 +19,17 @@ public class ProcessorImplTest {
     @Test
     public void testRun() {
         List<PositionPoint> fretBoard = (new FretBoard()).getPositions();
-        Processor processor = new ProcessorImpl(fretBoard);
+        List<ToneVertex> nodes = new ArrayList<ToneVertex>();
+        List<ToneEdge> edges = new ArrayList<ToneEdge>();
+        Processor processor = new ProcessorImpl(fretBoard, nodes, edges);
         
         List<Tone> phrase = DataBuilder.buildTestPhrase();
         
-        List<List<Position>> bestPositions = processor.run(phrase);
+        List<Position> bestPositions = processor.run(phrase);
         
+        for (Position position : bestPositions) {
+            System.out.println(position.toString());
+        }
         assertNotNull(bestPositions);
     }
     
